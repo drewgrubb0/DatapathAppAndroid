bb551e81a5833b45238bbc258a24bc382d33c1c8
Drew Grubb <36675950+drewgrubb0@users.noreply.github.com>
2018-04-14 19:49:33 -0500

    Added "Learn More" & "Try it yourself" buttons

--- src/main/java/com/termproject/cs3339/datapathproject/MainActivity.java
+++ src/main/java/com/termproject/cs3339/datapathproject/MainActivity.java
@@ -2,10 +2,12 @@ package com.termproject.cs3339.datapathproject;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
@@ -42,6 +44,34 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        Button learnMoreButton = (Button) findViewById(R.id.learnMoreButton);
+        learnMoreButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String link = "http://www.cise.ufl.edu/~mssz/CompOrg/CDA-proc.html";
+                Uri address = Uri.parse(link);
 
+                Intent travelIntent = new Intent(Intent.ACTION_VIEW, address);
+
+                if(travelIntent.resolveActivity(getPackageManager()) != null)
+                {
+                    startActivity(travelIntent);
+                }
+            }
+        });
+
+        Button tryItNowButton = (Button) findViewById(R.id.tryItYourself);
+        tryItNowButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Sends to DataPathActivity with no information
+                Intent showPathDisplay = new Intent(getApplicationContext(), DatapathActivity.class);
+                startActivity(showPathDisplay);
+            }
+        });
     }
 }
